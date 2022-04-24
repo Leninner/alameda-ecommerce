783e037933ade9b32a4c49d1adadfb7370b29577
leninner <mazabandalenin180@gmail.com>
2022-04-23 23:18:56 -0500

    Added product list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import { CategoriesFilter } from '../../components/CategoriesFilter'
+import { ProductList } from '../../components/ProductList'
 
 export const Home = () => {
   return (
     <div>
       <CategoriesFilter />
+      <ProductList />
     </div>
   )
 }
